6b859242b500e2221252d3333db3158faa2a49a6
Tomoro Saito <hg1190159@hiroogakuen.com>
2025-01-23 22:04:02 +0900

    toggletermのショートカットを追加

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -3,3 +3,17 @@ vim.keymap.set('t', '<C-w>h', '<C-\\><C-n><C-w>h', { noremap = true, silent = tr
 vim.keymap.set('t', '<C-w>j', '<C-\\><C-n><C-w>j', { noremap = true, silent = true })
 vim.keymap.set('t', '<C-w>k', '<C-\\><C-n><C-w>k', { noremap = true, silent = true })
 vim.keymap.set('t', '<C-w>l', '<C-\\><C-n><C-w>l', { noremap = true, silent = true })
+
+-- toggletermを開くショートカット
+function _G.toggle_or_open_terminal()
+  local terms = require("toggleterm.terminal")
+  local term = terms.get_all()[1]
+  if term and term:is_open() then
+      term:toggle()
+  else
+      vim.cmd('ToggleTerm')
+  end
+end
+
+-- Mapping Ctrl+T to toggle or open terminal
+vim.keymap.set('n', '<C-t>', ':lua toggle_or_open_terminal()<CR>', { noremap = true, silent = true })
