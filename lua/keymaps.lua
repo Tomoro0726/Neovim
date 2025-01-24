e6940855e58dd9cb2cd699ce001bea224a1b3ebb
Tomoro Saito <hg1190159@hiroogakuen.com>
2025-01-24 09:37:31 +0900

    いろいろ設定

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -2,9 +2,9 @@
 function _G.toggle_neotree()
   local neotree_visible = vim.bo.filetype == "neo-tree"
   if neotree_visible then
-      vim.cmd('Neotree close')
+    vim.cmd('Neotree close')
   else
-      vim.cmd('Neotree left reveal')
+    vim.cmd('Neotree left reveal')
   end
 end
 
@@ -15,15 +15,15 @@ function _G.toggle_terminal()
   local terms = require("toggleterm.terminal")
   local term = terms.get_all()[1]
   if term and term:is_open() then
-      term:toggle()
+    term:toggle()
   else
-      vim.cmd('ToggleTerm')
+    vim.cmd('ToggleTerm')
   end
 end
 
 vim.keymap.set('n', '<C-t>', ':lua toggle_terminal()<CR>', { noremap = true, silent = true })
 
-vim.keymap.set('t', '<Esc>', '<C-\\><C-n>')  -- Escape key exits terminal mode
+vim.keymap.set('t', '<Esc>', '<C-\\><C-n>') -- Escape key exits terminal mode
 vim.keymap.set('t', '<C-h>', '<C-\\><C-n><C-w>h')
 vim.keymap.set('t', '<C-j>', '<C-\\><C-n><C-w>j')
 vim.keymap.set('t', '<C-k>', '<C-\\><C-n><C-w>k')
@@ -33,10 +33,15 @@ vim.keymap.set('t', '<C-l>', '<C-\\><C-n><C-w>l')
 vim.api.nvim_create_autocmd("TermEnter", {
   pattern = "*",
   callback = function()
-      vim.keymap.set('t', 'jj', '<C-\\><C-n>', { buffer = true, noremap = true, silent = true })
+    vim.keymap.set('t', 'jj', '<C-\\><C-n>', { buffer = true, noremap = true, silent = true })
   end
 })
 
-
 -- Map to leader key + q
 vim.keymap.set('i', 'jj', '<Esc>', { noremap = true, silent = true })
+
+-- ウインドウ間の移動
+vim.keymap.set("n", "<C-h>", "<C-w>h", { silent = true })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { silent = true })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { silent = true })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { silent = true })
